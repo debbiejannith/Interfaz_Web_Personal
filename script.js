0e1a218e222a9a8eb347deab17f9b3be2f4db9ca
debbiejannith <76543278+debbiejannith@users.noreply.github.com>
2021-01-24 21:46:43 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+$('.capa-data').hide();
+$('.container-data').hide();
+$('#loadPhoto').hide();
+
+$('.container-data').css({
+    
+    bottom: '-450px'
+    
+});
+
+
+$('.request').on('click', function(){
+    
+        $('.capa-data').fadeIn();
+
+        $('.container-data').show();
+
+        $('.container-data').animate({
+
+            bottom: '0px'
+
+        });
+})
+
+$('.capa-data').on('click', function(){
+    
+    $(this).fadeOut();
+    
+     $('.container-data').animate({
+
+            bottom: '-450px'
+
+        });
+    
+    
+});
+
+
+$('.perfil-photo').on('click', function(){
+    $('#loadPhoto').click();
+})
+
+
+
+$(function() {
+  $('#loadPhoto').change(function(e) {
+      addImage(e); 
+     });
+
+     function addImage(e){
+      var file = e.target.files[0],
+      imageType = /image.*/;
+    
+      if (!file.type.match(imageType))
+       return;
+  
+      var reader = new FileReader();
+      reader.onload = fileOnload;
+      reader.readAsDataURL(file);
+     }
+  
+     function fileOnload(e) {
+      var result=e.target.result;
+      $('#photoSelect').attr("src",result);
+     }
+    });
+
+    window.setInterval (BlinkIt, 500);
+    var color = "while";  
+    function BlinkIt () {
+    var blink = document.getElementById ("blink");
+    color = (color == "#FFFFFF")? "white" : "#FFFFFF";
+    blink.style.color = color;
+    blink.style.fontSize='52px';}    
